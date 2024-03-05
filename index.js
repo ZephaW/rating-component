b8dd162b3c4b20f102b6091dd8e5f68c71d83e37
ZephaW <zwamala@gmail.com>
2024-03-05 12:32:50 +0300

    initialized index.JS

---
+++ index.js
@@ -0,0 +1,3 @@
+const ratingState = document.querySelector(".rating-state")
+const thankYou = document.querySelector(".thank-you-state")
+const submitButton = document.getElementById("submit-button")
