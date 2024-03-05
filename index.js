ae852efe92cc73ca138ccc8ea76d75eef60abb5a
ZephaW <zwamala@gmail.com>
2024-03-05 16:17:05 +0300

    changes made to index.JS

--- index.js
+++ index.js
@@ -1,21 +1,28 @@
 const ratingState = document.querySelector(".rating-state")
 const thankYou = document.querySelector(".thank-you-state")
-const submitButton = document.getElementById("submit-button")
-const rating = document.getElementById("rating")
-const rates = document.querySelector("rating-button")
+const submitButton = document.getElementById("submit")
+const rateAgain = document.getElementById("rate-again") 
+const ratings = document.querySelectorAll(".rating-button")
+const actualRating = document.getElementById("rating")
+
 
 submitButton.addEventListener("click", () => {
     thankYou.classList.remove("hidden")
     ratingState.style.display = "none"
+
+
+
+
+ratings.forEach((rating) => {
+    rate.addEventListener("click", () => {
+        actualRating.innerHTML = rating.innerHTML
+    })
 })
+
+})
+
 
 rateAgain.addEventListener("click", () => {
     thankYou.classList.add("hidden")
     ratingState.style.display = "block"
 })
-
-rates.forEach((rate) => {
-    rate.addEventListener("click", () => {
-        rating.innerHTML = rate.innerHTML
-    });
-});
