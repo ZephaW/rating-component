aaca90f97812ea17a45d78b336af62de82de134f
ZephaW <zwamala@gmail.com>
2024-03-05 13:45:54 +0300

    completed index.JS

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
 const ratingState = document.querySelector(".rating-state")
 const thankYou = document.querySelector(".thank-you-state")
 const submitButton = document.getElementById("submit-button")
+const rating = document.getElementById("rating")
+const rates = document.querySelector("rating-button")
 
 submitButton.addEventListener("click", () => {
     thankYou.classList.remove("hidden")
     ratingState.style.display = "none"
 })
+
+rateAgain.addEventListener("click", () => {
+    thankYou.classList.add("hidden")
+    ratingState.style.display = "block"
+})
+
+rates.forEach((rate) => {
+    rate.addEventListener("click", () => {
+        rating.innerHTML = rate.innerHTML
+    });
+});
