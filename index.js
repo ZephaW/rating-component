562d591c25490e7f77ceb2070d7098ba7cdb4964
ZephaW <zwamala@gmail.com>
2024-03-05 13:09:19 +0300

    added rate-again button

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
 const ratingState = document.querySelector(".rating-state")
 const thankYou = document.querySelector(".thank-you-state")
 const submitButton = document.getElementById("submit-button")
+
+submitButton.addEventListener("click", () => {
+    thankYou.classList.remove("hidden")
+    ratingState.style.display = "none"
+})
